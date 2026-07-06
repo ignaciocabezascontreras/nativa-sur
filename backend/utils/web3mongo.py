4b2c8a8a19d8a252a6cdab94ff896adf0896e06f
Ignacio Cabezas <ignaciocabezascontreras@gmail.com>
2026-07-05 22:31:30 -0400

    Eliminar web3mongo.py - causa error de importacion en Render

--- backend/utils/web3mongo.py
+++
@@ -1,6 +0,0 @@
-import os
-from pymongo import MongoClient
-
-MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
-client = MongoClient(MONGO_URI)
-db = client[os.getenv("MONGO_DB", "revision_narrativa_db")]
